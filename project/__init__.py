69074c3d4b01fd0e8e6fec1a86a70e0953bbf8d5
Wolfgang Black <wolfgangjblack@gmail.com>
2025-02-11 16:57:34 -0800

    added data utils import

--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,5 @@
 from .motion import SimpleMotionModel
+from .data_utils import extract_frames
 from .data_classes import Track, Detection
 from .eval_utils import read_positions_from_file, calculate_iou
 from .pipeline_utils import FeatureExtractor, VehicleTracker, load_config
